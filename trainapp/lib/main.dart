16276c78095a6ddca30c5ca78acdc537f9882a62
Shenith404 <139686889+Shenith404@users.noreply.github.com>
2024-01-05 11:32:27 +0530

    5/1
    plugin firebase

--- trainapp/lib/main.dart
+++ trainapp/lib/main.dart
@@ -1,7 +1,5 @@
 import "package:flutter/material.dart";
-import "package:trainapp/Pages/Auth/LoginOrRegister.dart";
 import "package:trainapp/Pages/RoutePage.dart";
-
 
 void main(List<String> args) {
   runApp(MyApp());
@@ -12,6 +10,9 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(debugShowCheckedModeBanner: false, home: RoutePage());
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: RoutePage(),
+    );
   }
 }
